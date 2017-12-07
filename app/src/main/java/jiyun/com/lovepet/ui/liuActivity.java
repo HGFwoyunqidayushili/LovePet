d64f12b457c1d7720278d018a3f38cd23dd096cc
liurongb <1270969067@qq.com>
2017-12-07 20:05:55 +0800

    刘荣斌fasong2222222222222222222222222

--- app/src/main/java/jiyun/com/lovepet/ui/liuActivity.java
+++ app/src/main/java/jiyun/com/lovepet/ui/liuActivity.java
@@ -11,5 +11,6 @@ public class liuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liu);
+        //再次提交!
     }
 }
