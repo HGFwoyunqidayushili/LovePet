64106276a053a4e0292373858e93f31d548ceeee
zhang <1524832680@qq.com>
2017-12-07 19:15:48 +0800

    zhanghuirong 2017/12/07

---
+++ app/src/main/java/jiyun/com/lovepet/ui/pet/activity/ToolbarActivity.java
@@ -0,0 +1,17 @@
+package jiyun.com.lovepet.ui.pet.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import jiyun.com.lovepet.R;
+
+
+public class ToolbarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toolbar);
+
+    }
+}
