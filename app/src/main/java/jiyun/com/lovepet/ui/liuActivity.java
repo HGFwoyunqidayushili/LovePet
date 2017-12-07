b87f05146eccfbf48b563aa83bec3e4b28e21fa0
liurongb <1270969067@qq.com>
2017-12-07 19:47:02 +0800

    刘荣斌fasong

---
+++ app/src/main/java/jiyun/com/lovepet/ui/liuActivity.java
@@ -0,0 +1,15 @@
+package jiyun.com.lovepet.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import jiyun.com.lovepet.R;
+
+public class liuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_liu);
+    }
+}
