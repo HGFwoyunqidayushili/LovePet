6690e9c219734943186253f0a2b147dc623bc4c1
liurongb <1270969067@qq.com>
2017-12-12 08:38:24 +0800

    刘荣斌提交代碼!2

--- app/src/main/java/jiyun/com/lovepet/ui/HomeActivity.java
+++ app/src/main/java/jiyun/com/lovepet/ui/HomeActivity.java
@@ -1,15 +1,19 @@
 package jiyun.com.lovepet.ui;
 
+import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ListView;
+import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,12 +22,11 @@ import android.widget.ToggleButton;
 import java.util.ArrayList;
 
 import jiyun.com.lovepet.R;
-import jiyun.com.lovepet.mvp.presenter.HomePresenter;
-import jiyun.com.lovepet.mvp.view.HomeView;
-import jiyun.com.lovepet.ui.adapter.HomeAdapter;
+
+import jiyun.com.lovepet.ui.pet.activity.MapActivity;
 import jiyun.com.lovepet.utils.CustomTextLayout;
 
-public class HomeActivity extends BaseActivity implements HomeView {
+public class HomeActivity extends BaseActivity  {
     private ArrayList<String> strings = new ArrayList<>();
     private ArrayList<String> strings1 = new ArrayList<>();
 
@@ -31,63 +34,93 @@ public class HomeActivity extends BaseActivity implements HomeView {
     private EditText editText;
     private ImageView imageView;
     private DrawerLayout draw;
-    private ListView listView1;
-    private ListView listView2;
-    private TextView fuJin;
-    private TextView jiGE;
     private ToggleButton shaixuan1;
     private ToggleButton shaixuan2;
     private View viewById1;
+    private LinearLayout linearLayout;
+    private ToggleButton shaixuan3;
+    private PopupWindow popupWindow3;
+    private ImageView imagetIntentToMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getLayoutId());//加载布局
         initView();//寻找控件
-        HomePresenter homePresenter = new HomePresenter(this);
-        homePresenter.getgetDatatoView();
         initData();//点击跳转到各个界面
         initLinnser();//用户点击listView事件
+
+    }
+
+    private void initPopupOne() {
+
     }
 
     private void initLinnser() {
-       listView1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-           @Override
-           public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-               fuJin.setText(strings1.get(i));
 
-           }
-       });
-        listView2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                jiGE.setText(strings.get(i));
-
-            }
-        });
         shaixuan1.setOnClickListener(new View.OnClickListener() {
+
+            private PopupWindow popupWindow;
+
             @Override
             public void onClick(View view) {
                 if (shaixuan1.isChecked()){
-                    listView1.setVisibility(View.VISIBLE);
+                    View popupview = LayoutInflater.from(HomeActivity.this).inflate(R.layout.mypopupwindow1, null);
+                    popupWindow = new PopupWindow(popupview, ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+                    popupWindow.setFocusable(true);
+                    popupWindow.setBackgroundDrawable(new BitmapDrawable());
 
+
+                    popupWindow.showAsDropDown(linearLayout);
                 }else {
-                    listView1.setVisibility(View.GONE);
-
+                    popupWindow.dismiss();
                 }
             }
         });
-        shaixuan2.setOnClickListener(new View.OnClickListener() {
+            shaixuan2.setOnClickListener(new View.OnClickListener() {
+
+                private PopupWindow popupWindow2;
+
+                @Override
+                public void onClick(View view) {
+                    if (shaixuan2.isChecked()){
+                        View popupview = LayoutInflater.from(HomeActivity.this).inflate(R.layout.mypopupwindow2, null);
+                        popupWindow2 = new PopupWindow(popupview, ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+                        //设置pop是否获取焦点
+                        popupWindow2.setFocusable(true);
+                        popupWindow2.setBackgroundDrawable(new BitmapDrawable());
+//http://123.56.150.230:8885/dog_family/t_user_info
+
+                        popupWindow2.showAsDropDown(linearLayout);
+                    }else {
+                         popupWindow2.dismiss();
+                    }
+                }
+            });
+        shaixuan3.setOnClickListener(new View.OnClickListener() {
+
+            private TextView intentText;
+
             @Override
             public void onClick(View view) {
+                if (shaixuan3.isChecked()){
+                    View popupview = LayoutInflater.from(HomeActivity.this).inflate(R.layout.mypopupwindow3, null);
+                    popupWindow3 = new PopupWindow(popupview, ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+                    popupWindow3.setFocusable(true);
+                    popupWindow3.setBackgroundDrawable(new BitmapDrawable());
 
 
-                if (shaixuan2.isChecked()){
+                    popupWindow3.showAsDropDown(linearLayout);
+                    intentText = popupview.findViewById(R.id.intent);
 
-                    listView2.setVisibility(View.VISIBLE);
-
+                    intentText.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Toast.makeText(HomeActivity.this, "跳转到筛选界面", Toast.LENGTH_SHORT).show();
+                        }
+                    });
                 }else {
-                    listView2.setVisibility(View.GONE);
+                    popupWindow3.dismiss();
                 }
             }
         });
@@ -98,7 +131,6 @@ public class HomeActivity extends BaseActivity implements HomeView {
     protected void initView() {
         CustomTextLayout customTextLayout = (CustomTextLayout) findViewById(R.id.App_title);
         editText = (EditText) findViewById(R.id.editText);
-        imageView = (ImageView) findViewById(R.id.imageView);
         draw = (DrawerLayout) findViewById(R.id.draw);
         nav_view = (NavigationView) findViewById(R.id.nav_view);
         nav_view.inflateHeaderView(R.layout.header_layout);
@@ -107,8 +139,6 @@ public class HomeActivity extends BaseActivity implements HomeView {
         //用户点击登录界面
         initLogin();
 
-        listView1 = (ListView) findViewById(R.id.listView1);
-        listView2 = (ListView) findViewById(R.id.listView2);
     }
 
 
@@ -128,13 +158,22 @@ public class HomeActivity extends BaseActivity implements HomeView {
         ImageView image = viewById1.findViewById(R.id.userXinXi);
         shaixuan1 = viewById1.findViewById(R.id.shaixuan1);
         shaixuan2 = viewById1.findViewById(R.id.shaixuan2);
-        fuJin = viewById1.findViewById(R.id.fuJin);
-        jiGE = viewById1.findViewById(R.id.jiaGe);
+        shaixuan3 = viewById1.findViewById(R.id.shaixuan3);
+        linearLayout = viewById1.findViewById(R.id.ll_home);
+        imagetIntentToMap = viewById1.findViewById(R.id.intentTo_Map);
 
+        //跳转到用户登录界面
         image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(HomeActivity.this, "用户登录界面", Toast.LENGTH_SHORT).show();
+            }
+        });
+        //跳转到地图界面
+        imagetIntentToMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                 startActivity(new Intent(HomeActivity.this, MapActivity.class));
             }
         });
 
@@ -184,15 +223,5 @@ public class HomeActivity extends BaseActivity implements HomeView {
         return R.layout.activity_home;
     }
 
-    //用户筛选界面
-    @Override
-    public void getData(ArrayList<String> arrayList, ArrayList<String> arrayList2) {
-        strings.addAll(arrayList2);
-        strings1.addAll(arrayList);
-        HomeAdapter homeAdapter = new HomeAdapter(arrayList2, HomeActivity.this);
 
-        HomeAdapter homeAdapter1 = new HomeAdapter(arrayList, HomeActivity.this);
-        listView2.setAdapter(homeAdapter);
-        listView1.setAdapter(homeAdapter1);
-    }
 }
---
+++ app/src/main/java/jiyun/com/lovepet/ui/pet/activity/MapActivity.java
@@ -0,0 +1,54 @@
+package jiyun.com.lovepet.ui.pet.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
+
+import jiyun.com.lovepet.R;
+//这个界面是向用户展示地图界面
+public class MapActivity extends AppCompatActivity {
+
+    private MapView map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        initView();
+        map.onCreate(savedInstanceState);
+        AMap map = this.map.getMap();
+
+
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
+        map.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
+        map.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
+        map.onPause();
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
+        map.onSaveInstanceState(outState);
+    }
+
+
+    private void initView() {
+        map = (MapView) findViewById(R.id.map);
+    }
+}
